cc1ceda9738bbfe717929f4ecd3bd3bbcbc407da
kwiatkopi <kwiatkopi@users.noreply.github.com>
2024-01-29 21:04:50 +0100

    /Added hello_World folder

---
+++ hello_world/views.py
@@ -0,0 +1,18 @@
+from hello_world import app
+from hello_world.formater import get_formatted
+from hello_world.formater import SUPPORTED, PLAIN
+from flask import request
+
+my_name = "Beata"
+msg = "Hello World!"
+
+@app.route('/')
+def index():
+    output = request.args.get('output')
+    if not output:
+        output = PLAIN
+    return get_formatted(msg, my_name, output.lower())
+
+@app.route('/outputs')
+def supported_output():
+    return ", ".join(SUPPORTED)
